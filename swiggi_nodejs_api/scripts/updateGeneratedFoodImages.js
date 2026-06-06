acf851fa438489f31ccbfc420fec3e86f1c77113
familymany123 <ngocsang120920@gmail.com>
2026-06-06 10:25:05 +0700

    Map fallback images for seeded foods

--- swiggi_nodejs_api/scripts/updateGeneratedFoodImages.js
+++ swiggi_nodejs_api/scripts/updateGeneratedFoodImages.js
@@ -5,28 +5,45 @@ require("dotenv").config();
 const dbURI = process.env.MONGODB_URI || "mongodb://localhost:27017/swiggi";
 const baseApi = process.env.BASE_API || "http://127.0.0.1:3001";
 
-const slugs = [
-  "ga-rut-xuong-gion",
-  "ga-rut-xuong-sot-cay",
-  "com-ga-sot-cay",
-  "com-ga-rut-xuong",
-  "burger-ga-gion",
-  "burger-bo-pho-mai",
-  "mi-y-bo-bam",
-  "combo-burger-ga",
-  "combo-gia-dinh-6-mieng-ga",
-  "salad-bap-cai",
-  "pho-mai-que",
-  "coca-cola",
-  "tra-dao",
-  "tra-chanh",
-];
+const imageBySlug = {
+  "1-mieng-ga-gion": "generated-ga-rut-xuong-gion.png",
+  "2-mieng-ga-gion": "generated-ga-rut-xuong-gion.png",
+  "4-mieng-ga-gion": "generated-ga-rut-xuong-gion.png",
+  "1-mieng-ga-sot-cay": "generated-ga-rut-xuong-sot-cay.png",
+  "2-mieng-ga-sot-cay": "generated-ga-rut-xuong-sot-cay.png",
+  "ga-rut-xuong-gion": "generated-ga-rut-xuong-gion.png",
+  "ga-rut-xuong-sot-cay": "generated-ga-rut-xuong-sot-cay.png",
+  "com-ga-gion": "generated-com-ga-rut-xuong.png",
+  "com-ga-mam-toi": "generated-com-ga-rut-xuong.png",
+  "com-ga-sot-cay": "generated-com-ga-sot-cay.png",
+  "com-ga-rut-xuong": "generated-com-ga-rut-xuong.png",
+  "com-trang": "generated-com-ga-rut-xuong.png",
+  "burger-ga-gion": "generated-burger-ga-gion.png",
+  "burger-tom": "generated-burger-ga-gion.png",
+  "burger-bo-pho-mai": "generated-burger-bo-pho-mai.png",
+  "combo-sandwich": "generated-combo-burger-ga.png",
+  "mi-y-sot-cay": "generated-mi-y-bo-bam.png",
+  "mi-y-bo-bam": "generated-mi-y-bo-bam.png",
+  "mi-y-ga-rut-xuong": "generated-mi-y-bo-bam.png",
+  "combo-mi-ga": "generated-combo-gia-dinh-6-mieng-ga.png",
+  "combo-ga-gion-khoai-nuoc": "generated-combo-gia-dinh-6-mieng-ga.png",
+  "combo-ga-cay-khoai-nuoc": "generated-combo-gia-dinh-6-mieng-ga.png",
+  "combo-burger-ga": "generated-combo-burger-ga.png",
+  "combo-gia-dinh-6-mieng-ga": "generated-combo-gia-dinh-6-mieng-ga.png",
+  "khoai-tay-chien": "generated-pho-mai-que.png",
+  "khoai-tay-lac-bbq-vua": "generated-pho-mai-que.png",
+  "salad-bap-cai": "generated-salad-bap-cai.png",
+  "pho-mai-que": "generated-pho-mai-que.png",
+  "coca-cola": "generated-coca-cola.png",
+  "tra-dao": "generated-tra-dao.png",
+  "tra-chanh": "generated-tra-chanh.png",
+};
 
 async function main() {
   await mongoose.connect(dbURI);
 
-  for (const slug of slugs) {
-    const image = `${baseApi}/uploads/generated-${slug}.png`;
+  for (const [slug, fileName] of Object.entries(imageBySlug)) {
+    const image = `${baseApi}/uploads/${fileName}`;
     const result = await Food.updateOne(
       { slug },
       { $set: { image, updated_at: new Date() } }
